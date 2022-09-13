bf26a5567a82fc93fc70fafc876f2c6e04ab67b6
danbi7184 <danbi7184@gmail.com>
2022-09-13 22:38:20 +0900

    first commit

---
+++ MMM-ToDoList.js
@@ -0,0 +1,64 @@
+Module.register("MMM-ToDoList", {
+	requiresVersion: "2.12.0",
+	default: {
+	},
+
+	getHeader: function () {
+		return "<i class='fa-regular fa-memo'></i> " + this.config.header;
+	},
+
+    getStyles: function () {
+        return ['MMM-ToDoList.css'];
+    },
+
+	start: function () {
+	  Log.info("Starting module: " + this.name);
+	},
+
+	getDom: function () {
+	  var wrapper = document.createElement("div");
+	  if (!this.loaded) {
+			return wrapper;
+	  }
+		var ToDoTable = document.createElement("table");
+		ToDoTable.className = "small";
+
+		var check = document.createElement("tr");
+		ToDoTable.appendChild(check);
+
+		var CheckInfo = this.CheckInfo;
+
+		var Todo1 = document.createElement("td");
+        Todo1.innerHTML = CheckInfo[0];
+        check.appendChild(Todo1);
+
+		 wrapper.appendChild(ToDoTable);
+		 return wrapper;
+	},
+
+	getListInfo: function () {
+	  Log.info("Requesting ToDoList");
+	  this.sendSocketNotification("GET_LIST");
+	},
+
+	notificationReceived: function (notification, payload, sender) {
+	  switch (notification) {
+		case "DOM_OBJECTS_CREATED":
+		  this.getListInfo();
+	  }
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+	  switch (notification) {
+		case "CHECKLIST":
+		  this.loaded = true;
+		  console.log("NotificationReceived:" + notification);
+		  this.CheckInfo = payload;
+		  this.updateDom();
+		  break;
+		case "CHECKLIST_ERROR":
+		  this.updateDom();
+		  break;
+	  }
+	},
+  });
---
+++ node_helper.js
@@ -0,0 +1,42 @@
+const NodeHelper = require("node_helper");
+var admin = require("firebase-admin");
+var firestore = require("firebase-admin/firestore");
+
+var serviceAccount = require("./nodejs.json");
+
+const db = firestore.getFirestore();
+
+module.exports = NodeHelper.create({
+	start: function() {
+		console.log("Starting nodehelper: " + this.name);
+		admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount)
+		});
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+		if(notification === 'GET_LIST') {
+			this.config = payload;
+			firebase.initializeApp(payload.firebaseConfig);
+			this.getToDoList();
+			return true;
+		}
+		return false;
+	},
+
+	getToDoList: async function() {
+		let self = this;
+		db.collection("ToDoList").get().then((result) => {
+			result.forEach((doc) => {
+				self.sendSocketNotification("CHECK_LIST", doc.data().checked);
+			});
+		});
+	},
+
+	stop: function() {
+		firebase
+			.database()
+			.ref(this.config.databaseURL)
+			.off();
+	},
+});
