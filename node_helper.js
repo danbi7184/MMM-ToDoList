21d72a62eb927b33d7044c8ab6ad9ff5962d93e2
danbi7184 <danbi7184@gmail.com>
2022-09-17 20:56:41 +0900

    fix

--- node_helper.js
+++ node_helper.js
@@ -36,7 +36,8 @@ module.exports = NodeHelper.create({
 				check[i] = doc.data().checked;
 				i++;
 			});
-			self.sendSocketNotification("CHECK_LIST", list);
+			console.log(list);
+			self.sendSocketNotification("CHECKLIST", list);
 		});
 	},
 });
