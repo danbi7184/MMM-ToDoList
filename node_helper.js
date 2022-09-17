87ed6a47ef94c7269bc88d4e8c2b75fcaa4bd19c
danbi7184 <danbi7184@gmail.com>
2022-09-17 17:32:16 +0900

    fix

--- node_helper.js
+++ node_helper.js
@@ -2,7 +2,7 @@ const NodeHelper = require("node_helper");
 const admin = require("firebase-admin");
 const firestore = require("firebase-admin/firestore");
 
-const serviceAccount = require("./credentials.json");
+const serviceAccount = require("./ServiceAccount.json");
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
