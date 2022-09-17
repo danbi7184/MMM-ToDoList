8c6a67b9d4964f522aa1c535df36828ca550befb
danbi7184 <danbi7184@gmail.com>
2022-09-17 17:38:20 +0900

    fix

--- node_helper.js
+++ node_helper.js
@@ -4,13 +4,16 @@ const firestore = require("firebase-admin/firestore");
 
 const serviceAccount = require("./ServiceAccount.json");
 
+var db;
+
 module.exports = NodeHelper.create({
 	start: function() {
 		console.log("Starting nodehelper: " + this.name);
 		admin.initializeApp({
 			credential: admin.credential.cert(serviceAccount)
 		});
-		const db = firestore.getFirestore();
+
+		db = firestore.getFirestore();
 	},
 
 	socketNotificationReceived: function(notification) {
