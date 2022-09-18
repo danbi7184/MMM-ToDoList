c914aa7ebcdc1588222210b0bc1f52745abfe014
danbi7184 <danbi7184@gmail.com>
2022-09-18 17:14:14 +0900

    fix

--- MMM-ToDoList.js
+++ MMM-ToDoList.js
@@ -51,23 +51,23 @@ Module.register("MMM-ToDoList", {
 			if(check[i] == 'true') {
 				listArr[i].style.color = '#A5A5A5';
 				listArr[i].style.textDecoration = 'line-through';
-				// inputArr[i].checked = true;
+				inputArr[i].checked = true;
 			} else {
 				listArr[i].style.color = '#ffffff';
 				listArr[i].style.textDecoration = 'none';
 			}
 
-			// inputArr[i].onclick = () => {
-			// 	if(inputArr[i].checked) {
-			// 		inputArr[i].checked = false;
-			// 		listArr[i].style.color = '#ffffff';
-			// 		listArr[i].style.textDecoration = 'none';
-			// 	} else {
-			// 		inputArr[i].checked = true;
-			// 		listArr[i].style.color = '#A5A5A5';
-			// 		listArr[i].style.textDecoration = 'line-through';
-			// 	}
-			// }
+			inputArr[i].onclick = () => {
+				if(inputArr[i].checked) {
+					inputArr[i].checked = false;
+					listArr[i].style.color = '#ffffff';
+					listArr[i].style.textDecoration = 'none';
+				} else {
+					inputArr[i].checked = true;
+					listArr[i].style.color = '#A5A5A5';
+					listArr[i].style.textDecoration = 'line-through';
+				}
+			}
 
 			checkArr[i].appendChild(inputArr[i]);
 			RowArr[i].appendChild(checkArr[i]);
